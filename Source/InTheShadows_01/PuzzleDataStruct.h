fc597a9cc7d9fdc389b42d1e325591e47f189004
DimiOui <you@example.com>
2023-11-28 18:16:29 +0100

    DataStructure + Tutorial Example

---
+++ Source/InTheShadows_01/PuzzleDataStruct.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "PuzzleDataStruct.generated.h"
+
+USTRUCT()
+struct FPuzzleData : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	FPuzzleData() : bIsCompleted(false) {}
+	
+	UPROPERTY(EditAnywhere, Category = "Puzzle Data")
+	FName PuzzleID; // A unique identifier for the puzzle
+
+	UPROPERTY(EditAnywhere, Category = "Puzzle Data")
+	bool bIsCompleted; // The completion state of the puzzle
+};
